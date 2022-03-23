f7f764535cc7ad93438f735060adb9d441c6606d
Mateus Rodrigues <mateus.rteu@gmail.com>
2022-03-23 10:47:03 -0300

    Initial Commit

---
+++ Tests/NetworkCoreTests/NetworkCoreTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import NetworkCore
+
+final class NetworkCoreTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(NetworkCore().text, "Hello, World!")
+    }
+}
